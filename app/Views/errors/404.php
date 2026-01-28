fb78b33333a4e0d51cb0a4387b21385cf5871db1
elecctroman <55356201+elecctroman@users.noreply.github.com>
2026-01-28 20:37:07 +0300

    Delete app directory

--- app/Views/errors/404.php
+++
@@ -1,6 +0,0 @@
-<?php ob_start(); ?>
-<h1>Sayfa bulunamadı</h1>
-<p>Aradığınız sayfa mevcut değil.</p>
-<a href="/">Anasayfaya dön</a>
-<?php $content = ob_get_clean(); ?>
-<?php include __DIR__ . '/../layouts/auth.php'; ?>
